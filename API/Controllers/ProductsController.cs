d1865270b0e61321d802fb8980ed39215feb9167
Pawel Borowczyk <martinischweppes@gmail.com>
2021-09-17 16:22:08 +0200

    lots changes uff

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -2,19 +2,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using API.Dtos;
+using API.Errors;
 using AutoMapper;
 using Core.Entities;
 using Core.Interfaces;
 using Core.Specifications;
 using Infrastructure.Data;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class ProductsController : ControllerBase
+    public class ProductsController : BaseApiController
     {
         private readonly IGenericRepository<Product> _productsRepo;
         private readonly IGenericRepository<ProductBrand> _productBrandRepo;
@@ -37,6 +37,8 @@ namespace API.Controllers
         }
 
         [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status404NotFound)]
         // when we hit this endpoint we passing an id
         public async Task<ActionResult<ProductToReturnDto>> GetProduct(int id)
         {
@@ -46,6 +48,8 @@ namespace API.Controllers
             var product = await _productsRepo.GetEntityWithSpec(spec);
             // At this point, return the product with the types and the brands.
             // we go here to _productsRepo to get an entity with this specification and we pass specification we created along with expressions to our GenericRepository.cs, go there!
+            if(product == null) return NotFound(new ApiResponse(404));
+
             return _mapper.Map<Product, ProductToReturnDto>(product);
         }
 
